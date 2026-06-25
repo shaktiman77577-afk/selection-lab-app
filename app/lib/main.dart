547491ee7783c514ff51171400a3e72a587d5550
shaktiman77577-afk <shaktiman77577@gmail.com>
2026-06-25 11:32:55 +0000

    Fix main.dart encoding

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,1 +1,60 @@
-Donecat
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'core/theme/app_theme.dart';
+import 'core/constants/app_constants.dart';
+import 'data/providers/auth_provider.dart';
+import 'presentation/screens/auth/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const SelectionLabApp());
+}
+
+class SelectionLabApp extends StatefulWidget {
+  const SelectionLabApp({super.key});
+
+  @override
+  State<SelectionLabApp> createState() => _SelectionLabAppState();
+}
+
+class _SelectionLabAppState extends State<SelectionLabApp> {
+  ThemeMode _themeMode = ThemeMode.dark;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDark = prefs.getBool(AppConstants.themeKey) ?? true;
+    setState(() => _themeMode = isDark ? ThemeMode.dark : ThemeMode.light);
+  }
+
+  void toggleTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDark = _themeMode == ThemeMode.dark;
+    setState(() => _themeMode = isDark ? ThemeMode.light : ThemeMode.dark);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()..loadUser()),
+      ],
+      child: MaterialApp(
+        title: AppConstants.appName,
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: _themeMode,
+        home: SplashScreen(onToggleTheme: toggleTheme),
+      ),
+    );
+  }
+}
